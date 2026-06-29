16424862a679742d03f8810e541c490b4b3cd96b
xuefeng <t-ink@qq.com>
2026-06-29 19:56:47 +0800

    增加遍历启动脚本

---
+++ scripts/step1_run_text_encoders.sh
@@ -0,0 +1,1 @@
+TEXT_ENCODERS_DIR="$HOME/dev/kimodo/text_encoders" HF_ENDPOINT=https://huggingface.co conda run -n kimodo kimodo_textencoder
---
+++ scripts/step2_run_web_gui.sh
@@ -0,0 +1,1 @@
+TEXT_ENCODER_MODE=api TEXT_ENCODER_URL=http://127.0.0.1:9550/ LOCAL_CACHE=True HF_ENDPOINT=https://huggingface.co conda run -n kimodo kimodo_demo
